5dcc8c6d303baed5dca0b9433dfe95ae85693dd5
Sheriffdeen Yusuf <sherifdeenmuhammed@outlook.com>
2022-02-27 22:15:21 +0100

    feat: :sparkles: add state to bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo_app/router/router.dart';
 
 void main() {
@@ -10,60 +11,15 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: FlexThemeData.light(primary: Colors.blue),
-        routeInformationParser: router.routeInformationParser,
-        routerDelegate: router.routerDelegate
-        //     home: const MyHomePage(title: 'Todo App'),
-        );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return ProviderScope(
+      child: MaterialApp.router(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: FlexThemeData.light(primary: Colors.blue),
+          routeInformationParser: router.routeInformationParser,
+          routerDelegate: router.routerDelegate
+          //     home: const MyHomePage(title: 'Todo App'),
+          ),
     );
   }
 }
--- lib/pages/dashboard/view/dashboard_page.dart
+++ lib/pages/dashboard/view/dashboard_page.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/pages/dashboard/provider/bottom_bar_provider.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends ConsumerWidget {
   const DashboardPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bottomBarState = ref.watch(bottomBarProvider);
     return Scaffold(
-        bottomNavigationBar: BottomNavigationBar(items: [
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todos'),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todos'),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todos'),
-        ]),
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: bottomBarState,
+            onTap: (value) => ref.read(bottomBarProvider.state).state = value,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todos'),
+              BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todos'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.list), label: 'Settings'),
+            ]),
         body: Column());
   }
 }
