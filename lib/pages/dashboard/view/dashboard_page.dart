bf382ffbdb4b1013501f4ed9c2971bdd6ba2c81f
Sheriffdeen Yusuf <sherifdeenmuhammed@outlook.com>
2022-02-27 23:45:06 +0100

    fix: :bug: fix status bar color bug

--- lib/pages/dashboard/view/dashboard_page.dart
+++ lib/pages/dashboard/view/dashboard_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/pages/dashboard/provider/bottom_bar_provider.dart';
 import 'package:todo_app/pages/todos/view/todos_page.dart';
@@ -9,25 +10,31 @@ class DashboardPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final bottomBarState = ref.watch(bottomBarProvider);
-    return Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-            currentIndex: bottomBarState,
-            onTap: (value) => ref.read(bottomBarProvider.state).state = value,
-            items: [
-              BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todos'),
-              BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todos'),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.list), label: 'Settings'),
-            ]),
-        body: SafeArea(
-          child: IndexedStack(
-            index: bottomBarState,
-            children: [
-              TodosPage(),
-              Text('Hello Africa'),
-              Text('Hello Naija'),
-            ],
-          ),
-        ));
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+          /*   appBar: AppBar(
+            toolbarHeight: 0,
+          ), */
+          bottomNavigationBar: BottomNavigationBar(
+              currentIndex: bottomBarState,
+              onTap: (value) => ref.read(bottomBarProvider.state).state = value,
+              items: [
+                BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todos'),
+                BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todos'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.list), label: 'Settings'),
+              ]),
+          body: SafeArea(
+            child: IndexedStack(
+              index: bottomBarState,
+              children: [
+                TodosPage(),
+                Text('Hello Africa'),
+                Text('Hello Naija'),
+              ],
+            ),
+          )),
+    );
   }
 }
